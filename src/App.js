71aaac66c97f8088295f20e7797db7feb83e7c5f
Rich <web_dev2017@outlook.com>
2021-02-02 12:30:22 +0000

    test to check build works

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Test to see if it's working</p>
         <a
           className="App-link"
           href="https://reactjs.org"
