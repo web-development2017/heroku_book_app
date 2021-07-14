d9f8e345e893d0b3b1a47afa5663f68dd376f363
Rich <web_dev2017@outlook.com>
2021-07-14 21:16:22 +0100

    materialize M not defined in BtnFns

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "materialize-css/dist/css/materialize.min.css";
-import M from  'materialize-css/dist/js/materialize.min.js';
+// import M from 'materialize-css/dist/js/materialize.min.js';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import {
   BrowserRouter as Router,
   Route,
